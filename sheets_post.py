504db3464184735bc9681cc82a58d7d431d3bef3
Michal Migurski <mike@teczno.com>
2017-05-12 13:04:27 -0700

    Added real fields

--- sheets_post.py
+++ sheets_post.py
@@ -26,7 +26,8 @@ def post_form(service, sheet_id, formdata):
     # Sheets are named by U.S. state
     sheet_name = '{State} Responses'.format(**formdata)
 
-    fields = ['Timestamp', 'Name'] # get_fields(service, sheet_id, sheet_name)
+    fields = ['Timestamp', 'County', 'State', 'First', 'Last', 'Email',
+              'Zip (Home)', 'Zip (Work)', 'Practice Status', 'Link']
     values = [formdata.get(name, None) for name in fields]
     
     print('Fields:', json.dumps(fields))
@@ -57,10 +58,19 @@ def lambda_handler(event, context):
     service = make_service(creds)
     
     # Assemble form data
-    name = event.get('data', {}).get('name')
-    state = event.get('data', {}).get('state')
-    timestamp = str(datetime.datetime.utcnow())
-    formdata = dict(Timestamp=timestamp, Name=name, State=state)
+    event_data = event.get('data', {})
+    formdata = {
+        'Timestamp': str(datetime.datetime.utcnow()),
+        'County': event_data.get('county'),
+        'State': event_data.get('state'),
+        'First': event_data.get('first_name'),
+        'Last': event_data.get('last_name'),
+        'Email': event_data.get('email'),
+        'Zip (Home)': event_data.get('home_zip'),
+        'Zip (Work)': event_data.get('work_zip'),
+        'Practice Status': event_data.get('practice_status'),
+        'Link': event_data.get('link'),
+        }
     
     post_form(service, sheet_id, formdata)
     return {'Location': redirect_url}
@@ -69,7 +79,18 @@ def main(filename, sheet_id):
     with open(filename) as file:
         creds = json.load(file)
         service = make_service(creds)
-    formdata = dict(Timestamp=str(datetime.datetime.utcnow()), Name='Lionel Hutz', State='CA')
+    formdata = {
+        'Timestamp': str(datetime.datetime.utcnow()),
+        'County': 'Alameda',
+        'State': 'CA',
+        'First': 'Lionel',
+        'Last': 'Hutz',
+        'Email': 'lhutz@example.com',
+        'Zip (Home)': '94608',
+        'Zip (Work)': '94612',
+        'Practice Status': 'A-Okay',
+        'Link': 'https://example.com',
+        }
     return post_form(service, sheet_id, formdata)
 
 if __name__ == '__main__':
